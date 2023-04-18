d1b6f390548c2c3230ba5efe9b3aac41dfbfe267
Cristian Larrauri <cristianlarrauri1@gmail.com>
2023-04-17 23:39:14 -0300

    Refactorizando codigo

--- src/routes/index.routes.js
+++ src/routes/index.routes.js
@@ -1,9 +1,9 @@
 const { Router } = require("express");
 // Importar todos los routers;
 // Ejemplo: const authRouter = require('./auth.js');
-const pokemons = require("./pokemons");
-const types = require("./types");
-const filter = require("./filter");
+const pokemons = require("./pokemons.routes");
+const types = require("./types.routes");
+const filter = require("./filter.routes");
 
 const router = Router();
 
